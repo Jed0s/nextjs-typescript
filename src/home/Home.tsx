89e615d5a1dd5f0306f0e5b1583bae8d9fb26758
jed0s <ruslan.bulavka@gmail.com>
2021-01-30 23:46:49 +0200

    add notes

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,9 +1,17 @@
+import { useMemo } from "react";
 import styled from "styled-components";
 
 export const Home = () => {
+const notes = useMemo(() => ([
+  '1. Add path aliases'
+]), []);
+
   return (
     <Home.Container>
       <Home.Title>Not perfect, but okay</Home.Title>
+      {notes.map((note) => (
+        <Home.Note>{note}</Home.Note>
+      ))}
     </Home.Container>
   );
 };
@@ -11,6 +19,7 @@ export const Home = () => {
 Home.Container = styled.div`
   align-items: center;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   height: 100%;
   width: 100%;
@@ -18,4 +27,11 @@ Home.Container = styled.div`
 
 Home.Title = styled.h1`
   font-size: 30px;
+`;
+
+Home.Note = styled.p`
+  border-bottom: 1px solid black;
+  font-size: 16px;
+  text-align: center;
+  width: 500px;
 `;
